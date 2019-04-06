a6739073153fc738919ae939401c114cf271be59
Dongo <cy.ginevraperso@gmail.com>
2019-04-06 20:28:40 +0200

    Saisie d'adresse IP Terminé
    saisie de l'adresse ip avec vérification est terminé (fonctionnel)

--- main.py
+++ main.py
@@ -1,21 +1,51 @@
 """ L'obectif du programme est de donner l'adresse de réseau, le masque, de broadcast, la plus haute
-                             et la plus petite en partant d'une adresse ip en notation classless"""
+et la plus petite en partant d'une adresse ip en notation classless"""
 
+def convertisseur_octet_decimal_binaire( octet_decimal ):
+    octet_binaire = str()
+    for bit in range(7, -1, -1):
+        if (octet_decimal - 2**bit) >= 0 :
+            octet_binaire += '1'
+        else:
+            octet_binaire += '0'
+    return octet_binaire
+
+def verification_masque( masque ):
+    """ Verifie la validité du masque si ou renvoie de True sinon False """
+    if 0 < int(masque) < 32:
+        return True
+    return False
+
+def verification_adresse( adresse ):
+    """ Verifie la validité de l'adresse IP si ou renvoie de True sinon False """
+    if adresse.count(".") != 3:
+        return False
+    adresse_split = adresse.split(".")
+    for octet in adresse_split:
+        if ( int(octet) < 0 ) or ( int(octet) > 255 ):
+            return False
+    return True
+
+def verification(adresse):
+    if (adresse.find("/") == -1):
+        return False
+    adresse_split = adresse.split("/")
+    if ( not verification_adresse(adresse_split[0]) ) or ( not verification_masque(adresse_split[1]) ):
+        return False
+    return True
 
 def saisie_adresse_ip():
-    """cette fonction demande à l'utilisateur de saisire une adresse ip en notation classless au format :      X.X.X.X/Y
-    Conditions à remplirs :
-        - un octets n'est pas plus est compris dans l'intervalle 0 < 255
-        - un adresse ip s'écrit en décimal pointé
-        - la séparation ip / masque se fait avec un slash
-        - un masque ne peut négatif, nul ou égale à 32 
-    Si ce n'est pas le cas alors le programme boucle"""
+    """ Demande à l'utilisateur une adresse ip, renvoie sous forme de liste ["adress", "masque"] """
     adresse = input("Veuillez saisir une adresse en notation classless : ")
+    while verification(adresse) == False :
+            adresse = input("Veuillez saisir une adresse en notation classless : ")
+    adresse_masque_split = adresse.split("/")
+    return adresse_masque_split
 
 
 # Programme principal pour tester vos fonctions
 def main():
-    pass
+    saisie_adresse_ip()
 
 if __name__ == '__main__':
     main()
